63ee229e2e63c3746b85bd73927694286de073ce
Dan <77572373+igafshell@users.noreply.github.com>
2023-07-16 15:07:44 +0300

    some assets added + a bit of dev done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			screen: {
+				"max-md": { max: "765px" },
+				"max-sm": { max: "400px" },
+			},
+		},
 	},
-	plugins: [],
-}
+	plugins: [require("tailwind-hamburgers")],
+};
